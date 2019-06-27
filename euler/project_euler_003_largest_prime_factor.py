4ab51bc8c0c0ca4f6f636b8141f6d369a296099d
Dr Walrus <bempelise@gmail.com>
2019-06-27 23:25:16 +0100

    Added implementation of Pollard's Rho algorithm

--- euler/project_euler_003_largest_prime_factor.py
+++ euler/project_euler_003_largest_prime_factor.py
@@ -10,6 +10,45 @@ What is the largest prime factor of a given number N
 """
 
 
+def gcd(a, b):
+    while a % b != 0:
+        a, b = b, a % b
+    return b
+
+
+def get_factor(number):
+
+    x_fixed = 2
+    cycle_size = 2
+    x = 2
+    factor = 1
+
+    while factor == 1:
+        count = 1
+        while count <= cycle_size and factor <= 1:
+            x = (x*x + 1) % number
+            factor = gcd(x - x_fixed, number)
+            count += 1
+        cycle_size *= 2
+        x_fixed = x
+    return factor
+
+
+def largest_prime_factor(number):
+    if number in [1, 0]:
+        return None
+
+    while number % 2 == 0:
+        factor = 2
+        number /= 2
+
+    while int(number) > 1:
+        factor = get_factor(number)
+        while number % factor == 0:
+            number /= factor
+    return int(factor)
+
+
 def get_primes(limit):
     primes = []
     current = list(range(2, limit + 1))
@@ -19,19 +58,22 @@ def get_primes(limit):
         current[:] = [num for num in current if num % next_prime != 0]
     return primes
 
-
-def largest_prime_factor(number):
+def largest_prime_factor_old(number):
     primes = get_primes(number)
     for num in reversed(primes):
         if number % num == 0:
             return num
 
-
 def main():
     """ main """
-    # number = 13195
-    number = 10
+    number = 13195
+    number = 1
+    number = 25
+    while (largest_prime_factor(number) == largest_prime_factor_old(number)):
+        number += 1
+    print(number)
     print(largest_prime_factor(number))
+    print(largest_prime_factor_old(number))
 
 
 if __name__ == "__main__":
