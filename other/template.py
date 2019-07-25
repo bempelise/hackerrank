ae5c66ae1b4e5d5289fe539557f6ca2015061a82
bempelise <bempelise@gmail.com>
2019-07-25 23:42:51 +0100

    template: Added general template

---
+++ other/template.py
@@ -0,0 +1,20 @@
+"""
+HackerRank - Problem Name
+
+https://www.hackerrank.com/problem_URL
+
+Problem description
+"""
+
+
+def problem_name():
+    """ Solves the problem"""
+
+
+def main():
+    """ main """
+    print(problem_name())
+
+
+if __name__ == "__main__":
+    main()
