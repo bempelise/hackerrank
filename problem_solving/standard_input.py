bd413d95bf0fd93d49407662bb022c0361609ebf
bempelise <bempelise@gmail.com>
2020-05-16 22:34:05 +0100

    template: added standard input

---
+++ problem_solving/standard_input.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import sys
+
+sizes = sys.stdin.readline().strip().split(' ')
+array = sys.stdin.readline().strip().split(' ')
+A = set(sys.stdin.readline().strip().split(' '))
+B = set(sys.stdin.readline().strip().split(' '))
+
+print(sizes)
+print(array)
+print(A)
+print(B)
