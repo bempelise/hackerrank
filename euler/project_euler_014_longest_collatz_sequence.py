eb41637672891ec0bdce73eaa1ca652f8341606e
bempelise <bempelise@gmail.com>
2020-05-17 03:55:24 +0100

    solution: Project Euler 014

--- euler/project_euler_014_longest_collatz_sequence.py
+++ euler/project_euler_014_longest_collatz_sequence.py
@@ -23,15 +23,99 @@ If many possible such numbers are there print the maximum one.
 Note: Once the chain starts the terms are allowed to go above N.
 
 """
+CACHE_SIZE = 5000
+CACHE = [0] * CACHE_SIZE
+CACHE[0] = 1
 
 
-def longest_collatz_sequence():
-    """ Solves the problem"""
+def collatz(num):
+    if num < CACHE_SIZE and CACHE[num - 1] != 0:
+        return CACHE[num - 1]
+
+    if num % 2:
+        result = 1 + collatz(3*num + 1)
+    else:
+        result = 1 + collatz(num >> 1)
+    if num < CACHE_SIZE:
+        CACHE[num - 1] = result
+    return result
+
+
+for num in range(2, CACHE_SIZE):
+    collatz(num)
+
+SOLUTIONS = [
+    3732423,
+    3542887,
+    3064033,
+    2298025,
+    1723519,
+    1564063,
+    1501353,
+    1126015,
+    1117065,
+    837799,
+    837799,
+    626331,
+    511935,
+    410011,
+    230631,
+    216367,
+    156159,
+    142587,
+    106239,
+    77031,
+    52527,
+    35655,
+    35497,
+    34239,
+    26623,
+    23529,
+    17673,
+    17647,
+    13255,
+    10971,
+    6171,
+    3711,
+    2919,
+    2463,
+    2323,
+    2322,
+    2223,
+    1161,
+    871,
+    703,
+    667,
+    655,
+    654,
+    649
+]
 
 
 def main():
     """ main """
-    print(longest_collatz_sequence())
+    T = int(input())
+    for _ in range(T):
+        n = int(input())
+        print(longest_collatz_sequence(n))
+
+
+def longest_collatz_sequence(number):
+    """ Solves the problem"""
+    index = 0
+    while index < len(SOLUTIONS):
+        if number >= SOLUTIONS[index]:
+            return SOLUTIONS[index]
+        index += 1
+
+    longest = 1
+    index = 1
+    for i in range(1, number + 1):
+        current = CACHE[i - 1]
+        if current >= longest:
+            longest = current
+            index = i
+    return index
 
 
 if __name__ == "__main__":
