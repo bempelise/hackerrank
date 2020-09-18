5f7d15983647602c39e4cc65b861a05b30130de7
bempelise <bempelise@gmail.com>
2020-09-18 01:10:00 +0100

    tools: standard input

--- problem_solving/standard_input.py
+++ problem_solving/standard_input.py
@@ -1,12 +1,19 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 import sys
 
-sizes = sys.stdin.readline().strip().split(' ')
-array = sys.stdin.readline().strip().split(' ')
-A = set(sys.stdin.readline().strip().split(' '))
-B = set(sys.stdin.readline().strip().split(' '))
+info = sys.stdin.readline().strip().split(' ')
+print(info[0])
+print(list(map(int, info[1:])))
 
-print(sizes)
-print(array)
-print(A)
-print(B)
+# line = sys.stdin.readline().strip().split(' ')
+# size = int(sys.stdin.readline().strip().split(' ')[0])
+# action, size = sys.stdin.readline().strip().split(' ')
+# A = set(sys.stdin.readline().strip().split(' '))
+# B = set(sys.stdin.readline().strip().split(' '))
+# A = set(map(int, sys.stdin.readline().strip().split(' ')))
+
+# print(action)
+# print(size)
+# print(line)
+# print(A)
+# print(B)
