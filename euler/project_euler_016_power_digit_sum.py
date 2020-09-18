bf61e5b366e26a0b979485af155242e4ffb91011
bempelise <bempelise@gmail.com>
2020-09-18 01:52:19 +0100

    template: Project Euler 016

---
+++ euler/project_euler_016_power_digit_sum.py
@@ -0,0 +1,22 @@
+"""
+HackerRank - Problem Name
+
+https://www.hackerrank.com/contests/projecteuler/challenges/euler016/problem
+
+2^9 = 512 and the sum of its digits is 5 + 1 + 2 = 8.
+
+What is the sum of the digits of the number 2^N?
+"""
+
+
+def power_digit_sum():
+    """ Solves the problem"""
+
+
+def main():
+    """ main """
+    print(power_digit_sum())
+
+
+if __name__ == "__main__":
+    main()
