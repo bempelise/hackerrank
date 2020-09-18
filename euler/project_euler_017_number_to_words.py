72d47b9322be9a71908e9d6e8b7aa7c55e48b376
bempelise <bempelise@gmail.com>
2020-09-18 02:05:51 +0100

    template: Project Euler 017

---
+++ euler/project_euler_017_number_to_words.py
@@ -0,0 +1,31 @@
+"""
+HackerRank - Problem Name
+
+https://www.hackerrank.com/contests/projecteuler/challenges/euler017/problem
+
+The numbers 1 to 5 written out in words are One, Two, Three, Four, Five
+
+First character of each word will be capital letter for example:
+104382426112 is
+
+One Hundred Four Billion
+Three Hundred Eighty Two Million
+Four Hundred Twenty Six Thousand
+One Hundred Twelve
+
+Given a number, you have to write it in words.
+
+"""
+
+
+def number_to_words():
+    """ Solves the problem"""
+
+
+def main():
+    """ main """
+    print(number_to_words())
+
+
+if __name__ == "__main__":
+    main()
